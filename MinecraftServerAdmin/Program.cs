adfff3f5ed37d004a29feee7f656a698153a5620
Dirk Kok <the@dirkkok.nl>
2021-08-01 15:09:38 +0200

    tls hack (will clean this all up later)

--- MinecraftServerAdmin/Program.cs
+++ MinecraftServerAdmin/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -17,6 +18,12 @@ namespace MinecraftServerAdmin {
 			Host.CreateDefaultBuilder(args)
 				.ConfigureWebHostDefaults(webBuilder => {
 					webBuilder.UseStartup<Startup>();
+					webBuilder.UseKestrel(serverOptions => {
+						serverOptions.Listen(IPAddress.Any, 443, listenOptions => {
+							listenOptions.UseHttps("/etc/ssl/private/corsac.nl.pfx");
+						});
+						serverOptions.AddServerHeader = false;
+					});
 				});
 	}
 }
